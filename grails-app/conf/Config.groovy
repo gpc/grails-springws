f9ba958fe0c902b3fcad09b101ec17f8c81431d4
Russ Miles <russmiles>
2009-05-14 14:18:51 +0000

    Added note on the more complex WSDL export case.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,19 @@
+// An example of exporting the WSDL, in the simplest case, for the Holiday Endpoint
 springws.wsdl.Holiday.export
+
+// A more complex wsdl export could take the form of:
+//springws {
+//    wsdl {
+//        Holiday {
+//            wsdlName= 'Holiday-v2'
+//            xsds= '/hr/v2/schemas/Holiday.xsd'
+//            portTypeName = 'HolidayPort'
+//            serviceName = 'HolidayService'
+//            locationUri = grails.serverURL + '/services/hr/v2/Holiday'
+//            targetNamespace = 'http://mycompany.com/hr/v2/definitions'
+//        }
+//    }
+//}
 
 // log4j configuration
 log4j = {
