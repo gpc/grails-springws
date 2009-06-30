fd7e38387c8983782718f74aff7656e9f78414aa
Russ Miles <russmiles>
2009-06-30 15:38:59 +0000

    Released 0.2.2 of the Spring WS plugin

--- SpringwsGrailsPlugin.groovy
+++ SpringwsGrailsPlugin.groovy
@@ -37,7 +37,7 @@ import org.apache.commons.logging.LogFactory
  */
 class SpringwsGrailsPlugin {
     // the plugin version
-    def version = "0.2.1"
+    def version = "0.2.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
