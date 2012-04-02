6fc1af6b05ef9a1c82951895ff5b7e84c19579dd
Peter Ledbrook <peter@cacoethes.co.uk>
2012-04-02 08:36:59 +0100

    Reformat plugin descriptor for consistency.

--- SpringwsGrailsPlugin.groovy
+++ SpringwsGrailsPlugin.groovy
@@ -56,75 +56,69 @@ class SpringwsGrailsPlugin {
     def grailsVersion = "1.1 > *"
 
     // the other plugins this plugin depends on
-	// This dependency would be useful, but should be scoped to
-	// test only to avoid packaging problems... however there is currently
-	// no scoping for inter-plugin dependencies
+    // This dependency would be useful, but should be scoped to
+    // test only to avoid packaging problems... however there is currently
+    // no scoping for inter-plugin dependencies
     //def dependsOn = [functionalTest:'1.2.5 > *']
 
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-			'grails-app/endpoints/*',
-			'grails-app/conf/WsSecurityConfig.groovy',
-			'test/functional/*',
-			'test/unit/*',
-			'test/integration/*',
-			'soapui/*'
+            'grails-app/endpoints/*',
+            'grails-app/conf/WsSecurityConfig.groovy',
+            'test/functional/*',
+            'test/unit/*',
+            'test/integration/*',
+            'soapui/*'
     ]
 
     def author = "Russ Miles"
 
     def authorEmail = "russ@russmiles.com"
 
-   	def title = "This plugin adds contract driven web service capabilities to a Grails application."
+    def title = "Spring WS Plugin"
 
-    def description = '''\\
-    	Spring Web Services plugin allows your Grails application to provide and consume 
-    	contract-driven web services. Feature highlights include: 
-    	* New in 0.5.0: Added support for Web Services Security (WS-Security)
-		* New in 0.2.3: Bug fix release (see http://jira.codehaus.org/browse/GRAILSPLUGINS-1225)
-		* New in 0.2.2: Added configuration option to override default Endpoint-name-based strategy for mapping incoming XML payloads to endpoints
-		* New in 0.2.1: Fixed concurrency bug in DefaultEndpointAdapter (see http://jira.codehaus.org/browse/GRAILSPLUGINS-1208)
-		* New in 0.2.1: Upgraded to Spring Web Services 1.5.7 (see http://jira.codehaus.org/browse/GRAILSPLUGINS-1208)
-		* Endpoint Interceptors so that you can now introduce
-		  common logic in an unintrusive fashion across a specified range of endpoints
-		* You can export the wsdl for a given endpoint by, in the simplest case,  
-		  setting the springws.wsdl.<your endpoint name, without the Endpoint bit>.export
-		* The EndpointFunctionalTestCase offers a more groovy withEndpointRequest closure 
-		  that significantly tightens up your endpoint functional test code.
-        * On the service provision side, a first-class endpoint	artefact is introduced including 
-          functional tests for endpoints.
-        * When consuming services, a WebServiceTemplate is provided, in much the same vein as 
-          the Spring WebServiceTemplate, that integrates more naturally with the rest of your Groovy code.'''
+    def description = '''\
+Spring Web Services plugin allows your Grails application to provide and consume contract-driven web services. Feature highlights include: 
+* New in 0.5.0: Added support for Web Services Security (WS-Security)
+* New in 0.2.3: Bug fix release (see http://jira.codehaus.org/browse/GRAILSPLUGINS-1225)
+* New in 0.2.2: Added configuration option to override default Endpoint-name-based strategy for mapping incoming XML payloads to endpoints
+* New in 0.2.1: Fixed concurrency bug in DefaultEndpointAdapter (see http://jira.codehaus.org/browse/GRAILSPLUGINS-1208)
+* New in 0.2.1: Upgraded to Spring Web Services 1.5.7 (see http://jira.codehaus.org/browse/GRAILSPLUGINS-1208)
+* Endpoint Interceptors so that you can now introduce common logic in an unintrusive fashion across a specified range of endpoints
+* You can export the wsdl for a given endpoint by, in the simplest case, setting the springws.wsdl.<your endpoint name, without the Endpoint bit>.export
+* The EndpointFunctionalTestCase offers a more groovy withEndpointRequest closure that significantly tightens up your endpoint functional test code.
+* On the service provision side, a first-class endpoint artefact is introduced including functional tests for endpoints.
+* When consuming services, a WebServiceTemplate is provided, in much the same vein as the Spring WebServiceTemplate, that integrates more naturally with the rest of your Groovy code.'''
 
     def documentation = "http://grails.org/SpringWs+Plugin"
 
     def artefacts = [EndpointArtefactHandler, InterceptorsConfigArtefactHandler, WsSecurityConfigArtefactHandler]
 
-	def watchedResources = ["file:./grails-app/endpoints/**/*",
+    def watchedResources = ["file:./grails-app/endpoints/**/*",
                             "file:./grails-app/conf/*WsSecurityConfig.groovy"]
 
-	def loadAfter = ['acegi']
-	
-	def log = LogFactory.getLog(SpringwsGrailsPlugin)
-	
-	static final DEFAULT_WS_SECURITY_CONFIG_NAME = 'WsSecurityConfig'
-	
-	static final ENDPOINT_BEANS = { endpoint ->
-	    "${endpoint.fullName}"(endpoint.clazz) { bean ->
+    def loadAfter = ['acegi']
+    
+    def log = LogFactory.getLog(SpringwsGrailsPlugin)
+    
+    static final DEFAULT_WS_SECURITY_CONFIG_NAME = 'WsSecurityConfig'
+    
+    static final ENDPOINT_BEANS = { endpoint ->
+        "${endpoint.fullName}"(endpoint.clazz) { bean ->
             bean.singleton = true
             bean.autowire = "byName"
         }
     }
 
-	static final SECURITY_CONFIG_BEANS = { config ->
+    static final SECURITY_CONFIG_BEANS = { config ->
         "$config.fullName"(config.clazz) { bean ->
             bean.singleton = true
             bean.autowire = "byName"
         }
     }
 
-	static final INTERCEPTOR_BEANS = { interceptor ->
-	    "${interceptor.fullName}Class"(MethodInvokingFactoryBean) {
+    static final INTERCEPTOR_BEANS = { interceptor ->
+        "${interceptor.fullName}Class"(MethodInvokingFactoryBean) {
             targetObject = ref("grailsApplication", true)
             targetMethod = "getArtefact"
             arguments = [InterceptorsConfigArtefactHandler.TYPE, interceptor.fullName]
@@ -136,9 +130,9 @@ class SpringwsGrailsPlugin {
     }
 
     def doWithSpring = {
-		// Add each of the endpoints
-		for(endpointClass in application.getArtefacts(EndpointArtefactHandler.TYPE)) {
-		    def name= endpointClass.name
+        // Add each of the endpoints
+        for(endpointClass in application.getArtefacts(EndpointArtefactHandler.TYPE)) {
+            def name= endpointClass.name
             def callable = ENDPOINT_BEANS.curry(endpointClass)
             callable.delegate = delegate
             callable.call()
@@ -158,7 +152,7 @@ class SpringwsGrailsPlugin {
             }
         }
 
-		// Add ws-security for each of the applicable classes
+        // Add ws-security for each of the applicable classes
         for(wsSecurityConfigClass in application.wsSecurityConfigClasses){
             log.debug "found WS-Security configuration class: ${wsSecurityConfigClass.fullName}"
             def callable = SECURITY_CONFIG_BEANS.curry(wsSecurityConfigClass)
@@ -175,7 +169,7 @@ class SpringwsGrailsPlugin {
             bean.call()
         }
 
-		// if Spring Security is installed, add access decision beans
+        // if Spring Security is installed, add access decision beans
         def foundAcegi = PluginManagerHolder.getPluginManager().hasGrailsPlugin('acegi')
         if (foundAcegi) {
             wsSecurityRoleVoter(SpringwsGrailsPlugin.classLoader.loadClass('org.springframework.security.vote.RoleVoter')) {}
@@ -184,25 +178,25 @@ class SpringwsGrailsPlugin {
             }
             wsSecurityObjectDefinitionSource(WebServiceInvocationDefinitionSource) {}
         }
-		
-		// Add each of the interceptors
-		for(interceptorsClass in application.getArtefacts(InterceptorsConfigArtefactHandler.TYPE)) {
+        
+        // Add each of the interceptors
+        for(interceptorsClass in application.getArtefacts(InterceptorsConfigArtefactHandler.TYPE)) {
             def callable = INTERCEPTOR_BEANS.curry(interceptorsClass)
             callable.delegate = delegate
             callable.call()
-     	}
+         }
 
-		// Payload mapper
+        // Payload mapper
         "payloadRootQNameEndpointMapping"(ReloadablePayloadRootQNameEndpointMapping)
 
         // exception resolvers
         // domainSoapFaultExceptionResolver(DomainSoapFaultExceptionResolver){
-        // 		order = 100
-		// }
-		//
-		// simpleSoapExceptionResolver(SimpleSoapExceptionResolver){
-		// 		order = 200
-		// }
+        //         order = 100
+        // }
+        //
+        // simpleSoapExceptionResolver(SimpleSoapExceptionResolver){
+        //         order = 200
+        // }
     }
 
     def doWithApplicationContext = { applicationContext ->
@@ -210,8 +204,8 @@ class SpringwsGrailsPlugin {
     }
 
     def onChange = { event ->
-	    if (log.debugEnabled) log.debug("onChange: ${event}")
-	
+        if (log.debugEnabled) log.debug("onChange: ${event}")
+    
         if(event.source.toString().endsWith('Endpoint')) {
             def newEndpoint = event.application.addArtefact(EndpointArtefactHandler.TYPE, event.source)
             beans(ENDPOINT_BEANS.curry(newEndpoint)).registerBeans(event.ctx)
@@ -221,11 +215,11 @@ class SpringwsGrailsPlugin {
         } else if (event.source.toString().endsWith('WsSecurityConfig')) {
             def newWsSecurityConfig = event.application.addArtefact(WsSecurityConfigArtefactHandler.TYPE, event.source)
             beans(SECURITY_CONFIG_BEANS.curry(newWsSecurityConfig)).registerBeans(event.ctx)
-		}
+        }
 
         reload(event.application, event.ctx)
     }
-	
+    
     private reload(GrailsApplication application, applicationContext) {
         log.info("reloadEndpoints")
         def defaultMappings = [:]
@@ -270,22 +264,22 @@ class SpringwsGrailsPlugin {
             if (acegiActive) {
                 log.debug 'Spring Security plugin is active.'
 
-            	if(foundSecurityBeans) {
-	                authenticationManager = applicationContext.authenticationManager
-	                userDetailsService = applicationContext.userDetailsService
-	                accessDecisionManager = applicationContext.wsSecurityAccessDecisionManager
-	                userCache = applicationContext.userCache
-	                objectDefinitionSource = applicationContext.wsSecurityObjectDefinitionSource
-	                log.debug "Using authenticationManager: $authenticationManager"
-	                log.debug "Using userDetailsService: $userDetailsService"
-	                log.debug "Using userCache: $userCache"
-	                log.debug "Using accessDecisionManager: $accessDecisionManager"
-	                log.debug "Using objectDefinitionSource: $objectDefinitionSource"
+                if(foundSecurityBeans) {
+                    authenticationManager = applicationContext.authenticationManager
+                    userDetailsService = applicationContext.userDetailsService
+                    accessDecisionManager = applicationContext.wsSecurityAccessDecisionManager
+                    userCache = applicationContext.userCache
+                    objectDefinitionSource = applicationContext.wsSecurityObjectDefinitionSource
+                    log.debug "Using authenticationManager: $authenticationManager"
+                    log.debug "Using userDetailsService: $userDetailsService"
+                    log.debug "Using userCache: $userCache"
+                    log.debug "Using accessDecisionManager: $accessDecisionManager"
+                    log.debug "Using objectDefinitionSource: $objectDefinitionSource"
 
-					//TODO add checks on the type of configured authorization
+                    //TODO add checks on the type of configured authorization
                     objectDefinitionSource.urlMatcher = applicationContext.filterInvocationInterceptor.objectDefinitionSource.urlMatcher
-		
-					// copy urls that start with /services/ to our objectDefinitionSource
+        
+                    // copy urls that start with /services/ to our objectDefinitionSource
                     if (!acegiConfig.useRequestMapDomainClass && acegiConfig.requestMapString) {
                         log.debug 'Using requestMapString for authorization.'
                         applicationContext.filterInvocationInterceptor.objectDefinitionSource.requestMap.each {
@@ -295,39 +289,39 @@ class SpringwsGrailsPlugin {
                         }
                     }
 
-                	// replace password encoder
-                	log.warn 'Replacing digest by plain text password encoder. All passwords will be stored in clear text in the database.'
-	                def plaintextPasswordEncoderClass = SpringwsGrailsPlugin.classLoader.loadClass('org.springframework.security.providers.encoding.PlaintextPasswordEncoder')
-	                def encoder = plaintextPasswordEncoderClass.newInstance()
-	                applicationContext.daoAuthenticationProvider?.passwordEncoder = encoder
-	                applicationContext.authenticateService?.passwordEncoder = encoder
+                    // replace password encoder
+                    log.warn 'Replacing digest by plain text password encoder. All passwords will be stored in clear text in the database.'
+                    def plaintextPasswordEncoderClass = SpringwsGrailsPlugin.classLoader.loadClass('org.springframework.security.providers.encoding.PlaintextPasswordEncoder')
+                    def encoder = plaintextPasswordEncoderClass.newInstance()
+                    applicationContext.daoAuthenticationProvider?.passwordEncoder = encoder
+                    applicationContext.authenticateService?.passwordEncoder = encoder
 
-                	// exclude /services/** from the FilterChainProxy by providing a more specific url
-	                def filterChain = applicationContext.springSecurityFilterChain
-	                def serviceFilters = []
-	                def newChainMap = ['/services/**':serviceFilters] as LinkedHashMap
+                    // exclude /services/** from the FilterChainProxy by providing a more specific url
+                    def filterChain = applicationContext.springSecurityFilterChain
+                    def serviceFilters = []
+                    def newChainMap = ['/services/**':serviceFilters] as LinkedHashMap
 
-               	 	newChainMap.putAll(filterChain.filterChainMap)
-	                filterChain.filterChainMap = newChainMap
-	                log.debug "Excluded /services/** from the security filter chain. Resulting mapping: ${filterChain.filterChainMap}"
-            	 }
-				 else {
-	                //TODO better message: suggest running the create-auth-domains script
-	                log.warn 'Security beans not found. Make sure the acegi plugin is active and the authentication domain is generated.'
-	            }
-	        }
-        	else {
+                        newChainMap.putAll(filterChain.filterChainMap)
+                    filterChain.filterChainMap = newChainMap
+                    log.debug "Excluded /services/** from the security filter chain. Resulting mapping: ${filterChain.filterChainMap}"
+                 }
+                 else {
+                    //TODO better message: suggest running the create-auth-domains script
+                    log.warn 'Security beans not found. Make sure the acegi plugin is active and the authentication domain is generated.'
+                }
+            }
+            else {
                 log.debug "Spring Security plugin is not active."
             }
         } else {
-			log.debug 'Spring Security plugin not found.'
-		}
+            log.debug 'Spring Security plugin not found.'
+        }
 
         log.debug("Reloading security config")
-		def foundDefaultWsSecurityConfig = applicationContext."$DEFAULT_WS_SECURITY_CONFIG_NAME" && application.isArtefactOfType(DEFAULT_WS_SECURITY_CONFIG_NAME, 'WsSecurityConfig')
-		log.debug "Found default WS-Security config: $foundDefaultWsSecurityConfig"
+        def foundDefaultWsSecurityConfig = applicationContext."$DEFAULT_WS_SECURITY_CONFIG_NAME" && application.isArtefactOfType(DEFAULT_WS_SECURITY_CONFIG_NAME, 'WsSecurityConfig')
+        log.debug "Found default WS-Security config: $foundDefaultWsSecurityConfig"
         for(wsSecurityConfigClass in application.wsSecurityConfigClasses) {
-			log.debug "Processing ${wsSecurityConfigClass.dump()}" 
+            log.debug "Processing ${wsSecurityConfigClass.dump()}" 
             def config = applicationContext."${wsSecurityConfigClass.fullName}"
             log.debug "Creating WS-Security interceptor for ${config.class.name}. Default: ${config.class.name == DEFAULT_WS_SECURITY_CONFIG_NAME}"
             def params = [securityConfigClass: config]
@@ -346,14 +340,14 @@ class SpringwsGrailsPlugin {
             def referents = []
             for (endpointClass in application.endpointClasses) {
                 if (GrailsClassUtils.isStaticProperty(endpointClass.clazz, 'wsSecurity')) {
-                   	def endpointName = GrailsNameUtils.getPropertyName(endpointClass.name)
+                       def endpointName = GrailsNameUtils.getPropertyName(endpointClass.name)
                     def wsSecurity = endpointClass.clazz.wsSecurity
                     // check for default config class i.e. static wsSecurity = true
                     if ((wsSecurity instanceof Boolean)) {
                         if (wsSecurity) {
                             // check that a default config class exists
                             if (!foundDefaultWsSecurityConfig) {
-							}
+                            }
                             // if this is the default config, use it
                             if (config.class.name == DEFAULT_WS_SECURITY_CONFIG_NAME) {
                                 referents << endpointName                                
@@ -389,22 +383,22 @@ class SpringwsGrailsPlugin {
     }
     
     def doWithWebDescriptor = { xml ->
-		// servlets
-	    def servlets = xml.servlet
-	    servlets[servlets.size() - 1] + {
-	        servlet {
-	            'display-name'("web-services")
-	            'servlet-name'("web-services")
-	            'servlet-class'("org.springframework.ws.transport.http.MessageDispatcherServlet")
-	        }
-	    }
-	    // servlet mappings
-	    def servletMappings = xml.'servlet-mapping'
-	    servletMappings[servletMappings.size() - 1] + {
-	        'servlet-mapping' {
-	            'servlet-name'("web-services")
-	            'url-pattern'("/services/*")
-	        }
-	    }
+        // servlets
+        def servlets = xml.servlet
+        servlets[servlets.size() - 1] + {
+            servlet {
+                'display-name'("web-services")
+                'servlet-name'("web-services")
+                'servlet-class'("org.springframework.ws.transport.http.MessageDispatcherServlet")
+            }
+        }
+        // servlet mappings
+        def servletMappings = xml.'servlet-mapping'
+        servletMappings[servletMappings.size() - 1] + {
+            'servlet-mapping' {
+                'servlet-name'("web-services")
+                'url-pattern'("/services/*")
+            }
+        }
     }
 }
